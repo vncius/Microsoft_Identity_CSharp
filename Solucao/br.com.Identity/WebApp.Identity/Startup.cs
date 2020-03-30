36e8e1e0b296121b7724ed83cdbadbe0c9dd96d9
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-03-30 20:20:35 -0300

    Realizado ajustes de nomenclatura e adicionado configuração em Startup.cs

--- Solucao/br.com.Identity/WebApp.Identity/Startup.cs
+++ Solucao/br.com.Identity/WebApp.Identity/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WebApp.Identity.Models;
 
 namespace WebApp.Identity
 {
@@ -23,6 +24,8 @@ namespace WebApp.Identity
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddIdentityCore<MyUser>(options => { });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
