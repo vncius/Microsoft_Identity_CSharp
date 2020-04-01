ca566e1bf4a3ddc887c2f8e1cb37591ccc3cd2ac
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2020-04-01 20:18:43 -0300

    Criado classes que extendem do IdentityDbContext e do IdentityUser
    Essa extensão permite criar novos campos e consultas pelo dbcontext personalizadas

--- Solucao/br.com.Identity/WebApp.Identity/Models/MyUser.cs
+++ Solucao/br.com.Identity/WebApp.Identity/Models/MyUser.cs
@@ -1,15 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApp.Identity.Models
 {
-    public class MyUser
+    public class MyUser : IdentityUser
     {
-        public string Id { get; set; }
-        public string UserName { get; set; }
-        public string NormalizedUserName { get; set; }
-        public string PasswordHash { get; set; }
+        public string nomeCompleto { get; set; }
     }
 }
---
+++ Solucao/br.com.Identity/WebApp.Identity/MyUserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Identity.Models;
+
+namespace WebApp.Identity
+{
+    public class MyUserDbContext : IdentityDbContext<MyUser>
+    {
+        public MyUserDbContext(DbContextOptions<MyUserDbContext> options) : base(options)
+        { 
+        
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
